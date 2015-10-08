457332e755a8c31fd1c39c13493b6f787098b5b2
Adam Surak <adam@algolia.com>
2015-10-08 12:25:09 +0100

    Initial Mesos monitoring plugin

---
+++ Mesos.py
@@ -0,0 +1,22 @@
+import json
+import urllib2
+
+class Mesos (object):
+    def __init__(self, agentConfig, checksLogger, rawConfig):
+        self.agentConfig = agentConfig
+        self.checksLogger = checksLogger
+        self.rawConfig = rawConfig
+
+    def run(self):
+        try:
+            ip = self.rawConfig['Mesos'].get(
+                'ip','127.0.0.1')
+            port = self.rawConfig['Mesos'].get(
+                'port',5050)
+            #self.checksLogger.info(self.rawConfig)
+            metrics = urllib2.urlopen("http://%s:%s/metrics/snapshot" % (ip, port)).read()
+            #self.checksLogger.info(metrics)
+            data = json.loads(metrics)
+            return data
+        except:
+            return {}
